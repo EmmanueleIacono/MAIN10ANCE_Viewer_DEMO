e49db1413e327c86c10aae69309bffb10f8058e9
EmmanueleIacono <iacono.emmanuele@gmail.com>
2021-11-05 17:23:58 +0100

    struttura front-end per login e signup

---
+++ public/js/login-signup.js
@@ -0,0 +1,105 @@
+const aLogin = document.getElementById('login-a');
+const aSignup = document.getElementById('signup-a');
+const contenitoreSignup = document.getElementById('contenitoreSignup');
+const contenitoreLogin = document.getElementById('contenitoreLogin');
+
+aLogin.addEventListener('click', () => {
+    contenitoreSignup.style.display = 'none';
+    contenitoreLogin.style.display = 'block';
+});
+
+aSignup.addEventListener('click', () => {
+    contenitoreLogin.style.display = 'none';
+    contenitoreSignup.style.display = 'block';
+});
+
+// redirectSeLoggato();
+
+$(() => {
+    $('#formLogin').submit(async (ev) => {
+        ev.preventDefault();
+        const username = $('#userLogin').val();
+        const pw = $('#pwLogin').val();
+        const infoUser = {
+            username,
+            pw
+        }
+        console.log('login');
+        console.log(infoUser);
+        // try {
+        //     const res = await login(infoUser);
+        //     const resJson = await res.json();
+        //     if (res.status !== 200) {
+        //         mostraErrore(resJson.message);
+        //     }
+        //     else {
+        //         nascondiErrore();
+        //         localStorage.user_id = resJson.id;
+        //         alert(resJson.message);
+        //         window.location = `/utente.html`;
+        //     }
+        // }
+        // catch(e) {
+        //     mostraErrore(e);
+        // }
+    });
+    $('#formSignup').submit(async (ev) => {
+        ev.preventDefault();
+        const username = $('#userSignup').val();
+        const pw = $('#pwSignup').val();
+        const infoUser = {
+            username,
+            pw
+        }
+        console.log('signup');
+        console.log(infoUser);
+        // try {
+        //     const res = await signup(infoUser);
+        //     const resJson = await res.json();
+        //     if (res.status !== 200) {
+        //         mostraErrore(resJson.message);
+        //     }
+        //     else {
+        //         nascondiErrore();
+        //         alert(resJson.message);
+        //         window.location = `/login.html`;
+        //     }
+        // }
+        // catch(e) {
+        //     mostraErrore(e);
+        // }
+    });
+});
+
+function login(user) {
+    return fetch("/auth/login", {method: "POST", headers: {"content-type": "application/json"}, body: JSON.stringify(user)});
+}
+
+function signup(user) {
+    return fetch("/auth/signup", {method: "POST", headers: {"content-type": "application/json"}, body: JSON.stringify(user)});
+}
+
+async function logout() {
+    const resRaw = await fetch("/auth/logout", {method: "GET", headers: {"content-type": "application/json"} });
+    const res = await resRaw.json();
+    localStorage.removeItem('user_id');
+    alert(res.message);
+    window.location = '/';
+}
+
+function mostraErrore(errore) {
+    console.log(errore);
+    const h3Errore = $('#messaggio-errore');
+    h3Errore.text(errore);
+    h3Errore.show();
+}
+
+function nascondiErrore() {
+    $('#messaggio-errore').hide();
+}
+
+function redirectSeLoggato() {
+    if (localStorage.user_id) {
+        window.location = window.location = `/utente.html`;
+    }
+}
