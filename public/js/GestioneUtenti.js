9452da8425eaf285d17cd1793d04605df07c8fdc
EmmanueleIacono <iacono.emmanuele@gmail.com>
2021-11-17 17:13:02 +0100

    gesione utenti, collapse navbar

---
+++ public/js/GestioneUtenti.js
@@ -0,0 +1,110 @@
+const bottoneTabUtenti = document.getElementById('apriTabUtenti');
+const contenitoreGestioneUtenti = document.getElementById('contenitore-gestione-utenti');
+const utenteCorrente = document.getElementById('liUsernameNavbar').innerText;
+
+bottoneTabUtenti.addEventListener('click', popolaGestioneUtenti);
+
+async function popolaGestioneUtenti() {
+    contenitoreGestioneUtenti.innerHTML = '';
+    const infoUtenti = await getInfoGestioneUtenti();
+    const infoRuoli = await getRuoliEnum();
+    const tabellaUtenti = document.createElement('table');
+    creaHeadersTabellaUtenti(tabellaUtenti);
+    infoUtenti.forEach((utente, ind) => {
+        const trUtente = creaModuloGestioneUtenti(utente, ind, infoRuoli);
+        tabellaUtenti.appendChild(trUtente);
+    });
+    contenitoreGestioneUtenti.appendChild(tabellaUtenti);
+}
+
+function creaHeadersTabellaUtenti(tabella) {
+    const intestazioni = ['Utenti', 'E-mail', 'Ruolo'];
+    const tr = document.createElement('tr');
+    tabella.appendChild(tr);
+    intestazioni.forEach(int => {
+        const th = document.createElement('th');
+        th.innerText = int;
+        tr.appendChild(th);
+    });
+}
+
+function creaModuloGestioneUtenti(user, id, ruoli) {
+    const contenitoreUser = document.createElement('tr');
+    const tdUser = document.createElement('td');
+    const tdEmail = document.createElement('td');
+    const tdRuolo = document.createElement('td');
+    const selectRuolo = document.createElement('select');
+    const bottoneAggiornaRuolo = document.createElement('button');
+    tdRuolo.setAttribute('id', `${id}-${user.ruolo}`);
+    ruoli.forEach(r => {
+        const optionRuolo = document.createElement('option');
+        optionRuolo.setAttribute('value', r);
+        optionRuolo.innerText = r;
+        selectRuolo.appendChild(optionRuolo);
+    });
+    tdRuolo.appendChild(selectRuolo);
+    tdRuolo.appendChild(bottoneAggiornaRuolo);
+    tdUser.innerText = user.user;
+    tdEmail.innerText = user.email;
+    selectRuolo.value = user.ruolo;
+    if (user.user === utenteCorrente) {
+        selectRuolo.disabled = true;
+        bottoneAggiornaRuolo.disabled = true;
+    }
+    bottoneAggiornaRuolo.innerText = 'MODIFICA';
+    bottoneAggiornaRuolo.style.float = 'right';
+    bottoneAggiornaRuolo.addEventListener('click', () => {
+        const ruoloOld = tdRuolo.id.split('-')[1];
+        if (ruoloOld !== selectRuolo.value) {
+            nuovoRuoloUtente(tdUser.innerText, selectRuolo.value);
+        }
+    });
+    contenitoreUser.appendChild(tdUser);
+    contenitoreUser.appendChild(tdEmail);
+    contenitoreUser.appendChild(tdRuolo);
+    return contenitoreUser;
+}
+
+async function getInfoGestioneUtenti() {
+    const utentiRaw = await fetch("/g/utenti", {method: "GET", headers: {"content-type": "application/json"} });
+    const utenti = await utentiRaw.json();
+    return utenti;
+}
+
+async function getRuoliEnum() {
+    const ruoliRaw = await fetch("/g/ruoli", {method: "GET", headers: {"content-type": "application/json"} });
+    const ruoli = await ruoliRaw.json();
+    return ruoli;
+}
+
+async function nuovoRuoloUtente(user, ruolo) {
+    const userJson = {
+        user,
+        ruolo
+    }
+    const res = await updateRuoloUtente(userJson);
+    if (res) {
+        popolaGestioneUtenti();
+    }
+    else {
+        alert('Operazione fallita');
+        popolaGestioneUtenti();
+    }
+}
+
+async function updateRuoloUtente(infoJson) {
+    let ris;
+    try {
+        const risultatoRaw = await fetch("/g/ruoli/nuovo-ruolo", {method: "PATCH", headers: {"content-type": "application/json"}, body: JSON.stringify(infoJson) });
+        const risultato = await risultatoRaw.json();
+        ris = risultato.success;
+    }
+    catch(e) {
+        console.log("Errore nell'aggiornamento dei ruoli");
+        console.log(e);
+        ris = false;
+    }
+    finally {
+        return ris;
+    }
+}
--- routes/database/reqGestore.js
+++ routes/database/reqGestore.js
@@ -15,15 +15,58 @@ appG.get('/utenti', async (req, res) => {
     res.send(JSON.stringify(users));
 });
 
+// per testare la richiesta
+// fetch("/g/ruoli", {method: "GET", headers: {"content-type": "application/json"} }).then(a => a.json()).then(console.log)
+appG.get('/ruoli', async (req, res) => {
+    const ruoli = await getListaRuoli();
+    res.setHeader('content-type', 'application/json');
+    res.send(JSON.stringify(ruoli));
+});
+
+appG.patch('/ruoli/nuovo-ruolo', async (req, res) => {
+    let result = {}
+    try {
+        const reqJson = req.body;
+        await updateRuoloUtente(reqJson);
+        result.success = true;
+    }
+    catch(e) {
+        result.success = false;
+    }
+    finally {
+        res.setHeader('content-type', 'application/json');
+        res.send(JSON.stringify(result));
+    }
+});
+
 //////////          QUERY          //////////
 
 async function getUtenti() {
     try {
-        const results = await clientServ.query(`SELECT * FROM "utenti";`);
+        const results = await clientServ.query(`SELECT "user", "email", "ruolo" FROM "utenti" ORDER BY "user";`);
         return results.rows;
     }
     catch(e) {
         return [];
+    }
+}
+
+async function getListaRuoli() {
+    try {
+        const result = await clientServ.query(`SELECT ARRAY(SELECT "ruolo" FROM "ruoli") AS "roles";`);
+        return result.rows[0].roles;
+    }
+    catch(e) {
+        return [];
+    }
+}
+
+async function updateRuoloUtente(userJson) {
+    try {
+        await clientServ.query(`UPDATE "utenti" SET "ruolo" = ($1) WHERE "user" = ($2);`, [userJson.ruolo, userJson.user]);
+    }
+    catch(e) {
+        throw(e);
     }
 }
 
