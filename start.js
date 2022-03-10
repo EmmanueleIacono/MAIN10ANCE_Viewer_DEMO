12f971202343373e1644dc9788bb8a76a0dcb82e
EmmanueleIacono <iacono.emmanuele@gmail.com>
2022-03-11 00:25:00 +0100

    prova deploy n5

--- start.js
+++ start.js
@@ -25,9 +25,9 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 if (process.env.NODE_ENV === 'production') {
     // app.use(cors());
     app.use(express.static('client/dist'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    // });
 }
 else {
     app.use(express.static(path.join(__dirname, 'public')));
