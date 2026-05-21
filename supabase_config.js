76500f2a0de776c71efa5c30cf27834d6c1f8b9a
Emmanuele Iacono <iacono.emmanuele@gmail.com>
2026-05-21 12:50:57 +0200

    general deps updates

--- supabase_config.js
+++ supabase_config.js
@@ -1,9 +1,11 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const options = {
-  autoRefreshToken: true,
-  persistSession: true,
-  detectSessionInUrl: true
+  auth: {
+    autoRefreshToken: true,
+    persistSession: false,
+    detectSessionInUrl: false
+  }
 }
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, options);
